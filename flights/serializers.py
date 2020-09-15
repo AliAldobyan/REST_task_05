96f9056500a34f229892fa99bef4bc5a5cc07a5d
Ali Al Dobyan <ali.aldobyan@gmail.com>
2020-09-15 16:17:11 +0300

    REST_task_05 completed

--- flights/serializers.py
+++ flights/serializers.py
@@ -28,6 +28,17 @@ class UpdateBookingSerializer(serializers.ModelSerializer):
 		fields = ['date', 'passengers']
 
 
+# class AdminUpdateBookingSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Booking
+#         fields = ['date', 'passengers']
+
+class UserUpdateBookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ['passengers']
+
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
